5fbfbd8fce133db218f61926f2ed0b7e6b0b1e5e
vanlailaptrinh <122373814+vanlailaptrinh@users.noreply.github.com>
2024-03-20 23:26:54 +0700

    Update Graph.java

--- Graph.java
+++ Graph.java
@@ -1,12 +1,14 @@
 package ThucHanhBuoi1;
 
 import java.util.LinkedHashMap;
+import java.util.LinkedList;
 import java.util.Map;
 import java.util.Set;
 
 public abstract class Graph {
 
 	protected Map<String, Set<String>> adjList;
+	protected Boolean [] visited;
 
 	public Graph() {
 
@@ -24,13 +26,31 @@ public abstract class Graph {
 	public abstract int degree(String v);
 
 	public abstract int edges();
+	
+	public abstract Boolean checkSingleGraph ();
+	
+	public abstract void BFS (int v) ;
 
 	public void print() {
 		for (Map.Entry<String, Set<String>> entry : adjList.entrySet()) {
 			System.out.println(entry.getKey() + " | " + entry.getValue());
 		}
-
+	}
+	public void printEdges () {
+		LinkedList<String> list = new LinkedList<>();
+		for (Map.Entry<String, Set<String>> string : adjList.entrySet()) {
+			if (string.getValue().size()!= 0) {
+				for (String vertex : string.getValue()) {
+					String edge = string.getKey() + vertex ;
+					list.add(edge);
+				}
+			}
+ 		}
+		for (int i = 0; i < list.size(); i++) {
+			System.out.print(list.get(i)+" ,");
+		}
 	}
 
+	
 	
 }
