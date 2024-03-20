a96161842ef49bf6d385ef8f643cd41fc74b911f
vanlailaptrinh <122373814+vanlailaptrinh@users.noreply.github.com>
2024-03-20 23:24:35 +0700

    Update DrirectedGraph.java

--- DrirectedGraph.java
+++ DrirectedGraph.java
@@ -1,9 +1,18 @@
 package ThucHanhBuoi1;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Iterator;
+import java.util.LinkedList;
 import java.util.Map;
 import java.util.Map.Entry;
+import java.util.Queue;
+
+import ThucHanhBuoi2.ReadFile;
+
 import java.util.Set;
 
 public class DrirectedGraph extends Graph {
@@ -72,14 +81,8 @@ public class DrirectedGraph extends Graph {
 	}
 
 	public int Outdegree(String v) {
-		int sum = 0;
-		for (Map.Entry<String, Set<String>> entry : adjList.entrySet()) {
-			if (entry.getKey().equals(v)) {
-				sum += entry.getValue().size();
-			}
-
-		}
-		return sum;
+		
+		return adjList.get(v).size();
 
 	}
 
@@ -96,5 +99,82 @@ public class DrirectedGraph extends Graph {
 		return res;
 
 	}
+	public static ArrayList<String> read (String url){
+		int v = 0 ;
+		ArrayList<String> result = new ArrayList<>();
+		File file = new File(url) ;
+		try {
+			FileReader reader = new FileReader(file) ;
+			BufferedReader buffer = new BufferedReader(reader);
+			String line = buffer.readLine();
+			v = Integer.parseInt(line) ;
+			line = buffer.readLine();
+			while (line != null) {
+				result.add(line);
+				line = buffer.readLine();
+			}
+			
+			
+			
+		} catch (Exception e) {
+        e.printStackTrace();
+        
+		}
+		return result;
+	}
+	public void addInAdjList () {
+	    ArrayList<String> arr = ReadFile.read("./dsdt.txt");
+	    String[][] matrix = new String[arr.size()][];
+
+	    for (int i = 0; i < arr.size(); i++) {
+	        String line = arr.get(i);
+	        String[] elements = line.split(" ");
+
+	        matrix[i] = elements;
+	    }
+
+	    for (int i = 0; i < matrix.length; i++) {
+	        for (int j = 0; j < matrix[i].length; j++) {
+	           System.out.print(matrix[i][j]);
+	           
+	           
+	        }
+	        System.out.println(); 
+	    }
+	}
+	public static void main(String[] args) {
+		ReadFile rd = new ReadFile();
+		rd.print();
+	}
+
+	@Override
+	public Boolean checkSingleGraph() {
+		for (Map.Entry<String, Set<String>> entry : adjList.entrySet()) {
+			String keys = entry.getKey();
+			Set<String> value = entry.getValue();
+			for (String values : value) {
+				if (keys.equals(values)) {
+					return false ;
+				}
+			}
+		}
+		return true;
+	}
+
+	
+	@Override
+	public void BFS(int v) {
+		Queue<Integer> queue = new LinkedList<>();
+		visited[v] = true ;
+		queue.add(v);
+		while (!queue.isEmpty()) {
+			v = queue.poll();
+			System.out.println(v + " ");
+			
+		}
+		
+	}
+
+	
 
 }
